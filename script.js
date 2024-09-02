37343c77b7bfe4d700e6416dd7a897a3ae3ecc06
Joseph Pal <josephpal27@gmail.com>
2024-09-02 12:44:26 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // Functionality For Progress Bars
 let progressBar = document.querySelectorAll(".progress-box .line");
 let percent = document.querySelectorAll(".percent");
@@ -12,5 +11,43 @@ window.addEventListener("scroll", () => {
     percent.forEach(num => {
         num.style.opacity = '1';
     })
+  }
+});
+
+
+// Functionality For Footer Section
+let footIconBox = document.querySelectorAll('.footer-icon-row .footer-icon');
+let footIcon = document.querySelectorAll('.footer-icon-row .footer-icon i');
+
+footIconBox.forEach((box, i) => {
+    box.addEventListener('mouseenter', () => {
+        footIcon[i].style.color = '#f15c06';
+    })
+    box.addEventListener('mouseleave', () => {
+        footIcon[i].style.color = '#fff';
+    })
+})
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Functionality For Back to Top Btn
+let backToTopDiv = document.querySelector(".back-to-top");
+let backToTopBtn = document.querySelector(".back-to-top .fa-arrow-up");
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    // Adjust the pixel value as needed
+    backToTopDiv.classList.add("show");
+  } else {
+    backToTopDiv.classList.remove("show");
   }
 });
