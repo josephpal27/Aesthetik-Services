bf8f3ef29f4100615396c113fa149b351ca5b31c
Joseph Pal <josephpal27@gmail.com>
2024-09-06 13:08:56 +0530

    mobile responsiveness added

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ let progressBar = document.querySelectorAll(".progress-box .line");
 let percent = document.querySelectorAll(".percent");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 1600) {
-    progressBar.forEach((bar) => {
-      bar.style.width = "100%";
-    });
-    percent.forEach(num => {
-        num.style.opacity = '1';
-    })
+
+  if (screen.width < '576') {
+    if (window.scrollY > 3400) {
+      progressBar.forEach((bar) => {
+        bar.style.width = "100%";
+      });
+      percent.forEach(num => {
+          num.style.opacity = '1';
+      })
+    }
+  } else{
+    if (window.scrollY > 1600) {
+      progressBar.forEach((bar) => {
+        bar.style.width = "100%";
+      });
+      percent.forEach(num => {
+          num.style.opacity = '1';
+      })
+    }
   }
 });
 
